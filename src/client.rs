a76c31d6e848330f93188ef6c2640ced0a890a84
GYoung <ytok1108@aol.com>
2023-12-10 16:59:10 +0900

    Terminal::Clear

--- src/client.rs
+++ src/client.rs
@@ -1,10 +1,16 @@
-use std::{io::stdout, thread, time::Duration};
+use std::{
+    io::{stdout, Write},
+    thread,
+    time::Duration,
+};
 
-use crossterm::{cursor, QueueableCommand};
+use crossterm::{cursor, terminal, QueueableCommand};
 
 fn main() {
     println!("Hello Client");
     let mut stdout = stdout();
+    // stdout.queue(terminal::Clear(terminal::ClearType::All));
     stdout.queue(cursor::MoveTo(5, 5)).unwrap();
+    stdout.flush().unwrap();
     thread::sleep(Duration::from_secs(5));
 }
